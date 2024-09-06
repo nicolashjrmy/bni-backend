de35693915dc211c34dfa0fba7e1561fda9af028
nicolashjrmy <nicolashjeremy17@gmail.com>
2024-09-06 09:31:27 +0700

    add test file for pie chart

--- src/route/tester/tester.controller.ts
+++ src/route/tester/tester.controller.ts
@@ -17,16 +17,7 @@ export class TesterController {
   constructor(private readonly neo4jService: Neo4jService) {}
 
   @Get('graph-profil-buron')
-  async getGraphProfilBuron(
-    @Query('nik') nik: string,
-    @Query('no_hp') no_hp: string,
-    @Query('no_rek') no_rek: string,
-    @Query('start_date') start_date: string,
-    @Query('end_date') end_date: string,
-    @Query('email') email: string,
-    @Query('n_kontak1') n_kontak1: string,
-    @Query('tgl_cctv') tgl_cctv: string,
-  ) {
+  async getGraphProfilBuron() {
     const result = await this.neo4jService.read(
       `match p = ()--()
 
@@ -36,5 +27,28 @@ export class TesterController {
     const formatResult = formatResponse(result.records);
     return formatResult;
     // return result.records;
+  }
+
+  @Get('pie')
+  async getPie() {
+    const result = await this.neo4jService.read(
+      `match (n:Buronan{jenis_kelamin:'Perempuan'})
+       with count(n) as wanita
+       match (m:Buronan{jenis_kelamin: 'Laki-laki'})
+       return wanita, count(m) as pria`,
+    );
+    const pria = result.records[0].get('pria').toInt();
+    const wanita = result.records[0].get('wanita').toInt();
+    const total = pria + wanita;
+
+    const priaPercentage = ((pria / total) * 100).toFixed(2);
+    const wanitaPercentage = ((wanita / total) * 100).toFixed(2);
+
+    return {
+      Pria: pria,
+      Wanita: wanita,
+      PriaPercentage: `${priaPercentage}%`,
+      WanitaPercentage: `${wanitaPercentage}%`,
+    };
   }
 }
